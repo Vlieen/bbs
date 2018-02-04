f2351358ed94a32ac10c942c2ced218af71c0e28
Glieen <guohongjun2016@gmail.com>
2018-02-05 00:04:22 +0800

    Test Start

---
+++ src/main/java/com/glieen/Application.java
@@ -0,0 +1,18 @@
+package com.glieen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Glieen
+ * Date: 2018/2/4
+ * Time: 23:57
+ * Desc: 项目启动类
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/glieen/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.glieen.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Glieen
+ * Date: 2018/2/4
+ * Time: 23:58
+ * Desc: 项目测试
+ */
+@RestController
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+}
